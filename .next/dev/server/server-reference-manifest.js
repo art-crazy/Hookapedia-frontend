cc1e0bd6c7cd37264ad27d9ea0f84996ccc9ff4c
arzhannikov <artcrazy.mail@gmail.com>
2025-11-25 00:18:12 +0300

    fix

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jd4otddT0z3dKMRRIrEfFt3WpTlyVj8ZcfrGfiypFsE=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__79866fa4._.css",
-  "static/chunks/_3f24e0c2._.js"
-],
-    source: "dynamic"
-});
