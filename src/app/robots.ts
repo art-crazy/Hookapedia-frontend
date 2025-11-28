6818bbb2f51187e344eafd16742d4c43bc8be7eb
arzhannikov <artcrazh.mail@gmail.com>
2025-11-28 16:12:50 +0300

    fix(*): del seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,12 @@
 import { MetadataRoute } from 'next'
-import { siteConfig } from '@/config/site'
+// import { siteConfig } from '@/config/site'
 
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
-            allow: '/',
+            disallow: '/',
         },
-        sitemap: `${siteConfig.url.current}/sitemap.xml`,
+        // sitemap: `${siteConfig.url.current}/sitemap.xml`,
     }
 }
