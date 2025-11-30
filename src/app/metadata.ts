664b5650af19bed107193236838445d273e7d70b
arzhannikov <artcrazh.mail@gmail.com>
2025-11-30 14:54:15 +0300

    feat(*): add favicon

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -28,4 +28,15 @@ const baseMetadata = buildMetadata({
 
 export const metadata: Metadata = {
     ...baseMetadata,
+    icons: {
+        icon: [
+            { url: '/favicon.ico', sizes: '32x32' },
+            { url: '/favicon.svg', type: 'image/svg+xml' },
+            { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' }
+        ],
+        apple: [
+            { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+        ]
+    },
+    manifest: '/site.webmanifest'
 };
