6865ce87d7c84289f9cfb1d15bec92068ffc5945
arzhannikov <artcrazh.mail@gmail.com>
2025-11-30 17:58:42 +0300

    feat(*): fix error worker service

---
+++ public/pushwoosh-service-worker.js
@@ -0,0 +1,14 @@
+// Unregister old Pushwoosh service worker
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    self.registration.unregister().then(function() {
+      return self.clients.matchAll();
+    }).then(function(clients) {
+      clients.forEach(client => client.navigate(client.url));
+    })
+  );
+});
