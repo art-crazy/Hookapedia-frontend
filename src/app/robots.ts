60c59166ee1b34d9715173c16da711dc4159b9a1
arzhannikov <artcrazh.mail@gmail.com>
2025-11-30 16:39:27 +0300

    feat(*): откатил next до 15 версии

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,13 @@
 import { MetadataRoute } from 'next'
-// import { siteConfig } from '@/config/site'
+import { siteConfig } from '@/config/site'
 
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: '*',
-            disallow: '/',
+            allow: '/',
+            disallow: '/recepty/*',
         },
-        // sitemap: `${siteConfig.url.current}/sitemap.xml`,
+        sitemap: `${siteConfig.url.current}/sitemap.xml`,
     }
 }
